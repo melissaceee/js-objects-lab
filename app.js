ba2bb9d72a02269c54190df95d6881a9ce55ebb3
melissaceee <cheng.melissa@outlook.com>
2024-05-22 01:14:18 -0400

    exercise 12

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const game = {
     {number: 1, name: 'Bulbasaur', type: 'grass', hp: 45, starter: true},
     {number: 25, name: 'Pikachu', type: 'electric', hp: 35, starter: true},
     {number: 7, name: 'Squirtle', type: 'water', hp: 44, starter: true },
-    {number: 56, name: 'Mankey', type: 'fighting', hp: 40,starter: false}
+    {number: 56, name: 'Mankey', type: 'fighting', hp: 40,starter: false},
+    {number: 97, name: 'Hypno', type: 'psychic', hp: 85, starter: false}
     ],
     gyms: [
       { location: "Pewter City", completed: false, difficulty: 1 },
@@ -26,8 +27,11 @@ const game = {
         {level: "easy"},
         {level: "medium"},
         {level: "difficult"}
-    ]
-  }
+    ],
+    catchPokemon: function(pokemonObj){
+        this.party.push(pokemonObj);
+    }   
+};
 
 
 //   console.dir(pokemon, { maxArrayLength: null })
@@ -111,5 +115,118 @@ Solve Exercise 7 here:
 // console.log(game.gyms)
     
     
+ /*
+Exercise 8
+1. Evolve the starter Pokémon you added to your party earlier. Each starter Pokémon evolves into a specific one.
+2. How would you replace the current starter Pokémon in your party with its evolved form?
+
+Hint: 
+  - Pokemon 1: Bulbasaur evolves into Pokemon 2: Ivysaur
+  - Pokemon 4: Charmander evolves into Pokemon 5: Charmeleon
+  - Pokemon 7: Squirtle evolves into Pokemon 8: Wartortle
+  - Pokemon 25: Pikachu evolves into Pokemon 26: Raichu
+
+More Hints: The existing starter Pokemon will be *replaced* in your party with the Pokemon it evolved into. Remember that you're working with an array of objects - what array method is ideal for replacing one element with another? 
+
+Solve Exercise 8 here:
+*/
+
+// COME BACK TO  8
+
+
+/*
+Exercise 9
+1. Print the name of each Pokémon in your party.
+2. Consider using a loop or an array method to access each Pokémon's name.
+
+Solve Exercise 9 here:
+*/
+
+// for (let i = 0; i <game.party.length; i++) {
+//     console.log(game.party[i].name);
+// }
+
+
+/*
+Exercise 10
+1. Can you print out all the starter Pokémon from the `pokemon` array?
+2. Think about how you can identify a starter Pokémon and then log their names.
+
+
+Solve Exercise 10 here:
+*/
+
+// pokemon.forEach(pokemon =>{
+//     if (pokemon.starter === true) {
+//         console.log(pokemon.name)
+//     }
+// })
+
+
+/*
+Exercise 11
+1. Add a method called `catchPokemon` to the `game` object. This method should:
+  - Accept an object as a parameter called `pokemonObj`
+  - Add the `pokemonObj` to the `game.party` array.
+  - not return anything
+
+After writing this method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+
+Solve Exercise 11 here:
+*/
+
+// catchPokemon: function(pokemonObj){
+//     this.party.push(pokemonObj);
+// } 
+
+// const pokemonToCatch = {
+//     number: 11,
+//     name: "Metapod",
+//     type: "bug",
+//     hp: 50,
+//     starter: false
+// };
+
+// game.catchPokemon(pokemonToCatch);
+
+// console.log(game.party);
+
+
+
+/*
+Exercise 12
+1. Copy the `catchPokemon` method that you just wrote above, and paste it below. Modify it so that it also decreases the number of pokeballs in your inventory each time you catch a Pokémon.
+2. How will you find and update the quantity of pokeballs in the `game.items` array?
+
+Tips:
+For this exercise, it's okay to have a negative number of pokeballs.
+After updating the method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+Also, log the `game.items` array to confirm that the pokeball quantity is being decremented.
+
+Solve Exercise 12 here:
+*/
+
+
+// game.catchPokemon = function(pokemonObj) {
+//     this.party.push(pokemonObj); 
     
+
+//     const pokeballnum = this.items.findIndex(item => item.name === 'pokeball');
+
+//     if (pokeballnum !== -1) {
+//         this.items[pokeballnum].quantity--; 
+//     }
+// };
+
+// const pokemonToCatch = {
+//     number: 11,
+//     name: "Metapod",
+//     type: "bug",
+//     hp: 50,
+//     starter: false
+// };
+
+// game.catchPokemon(pokemonToCatch);
+
+// console.log(game.items);
 
